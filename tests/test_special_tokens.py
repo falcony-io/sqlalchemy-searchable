e9d5143b8751bee1d74a5cfebeca848225426d68
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-03-19 17:30:46 +0200

    Add test case for special tokens

---
+++ tests/test_special_tokens.py
@@ -0,0 +1,28 @@
+from tests import TestCase
+
+
+class SignedIntegerTokenTestCase(TestCase):
+    def setup_method(self, method):
+        TestCase.setup_method(self, method)
+        self.session.add(
+            self.TextItem(name=u'index', content=u'some 12-14')
+        )
+        self.session.commit()
+
+
+class TestSignedIntegersWithRemoveHyphens(SignedIntegerTokenTestCase):
+    remove_hyphens = True
+
+    def test_with_hyphen_search_term(self):
+        assert self.TextItemQuery(
+            self.TextItem, self.session
+        ).search('12-14').count()
+
+
+class TestSignedIntegersWithoutRemoveHyphens(SignedIntegerTokenTestCase):
+    remove_hyphens = False
+
+    def test_with_hyphen_search_term(self):
+        assert not self.TextItemQuery(
+            self.TextItem, self.session
+        ).search('12-14').count()
