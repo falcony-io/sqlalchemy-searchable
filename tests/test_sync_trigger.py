2738f651dca6f8febca9af67910a9cd42d261728
Janne Vanhala <jpvanhal@users.noreply.github.com>
2023-08-07 22:55:09 +0300

    test: wrap raw sql queries with text() construct
    Wrap the raw SQL queries passed to `Connection.execute()` with `text()`
    construct. This resolves the following deprecation warning:
    
        Passing a string to Connection.execute() is deprecated and will be
        removed in version 2.0.  Use the text() construct, or the
        Connection.exec_driver_sql() method to invoke a driver-level SQL
        string.
    
    Refs #111

--- tests/test_sync_trigger.py
+++ tests/test_sync_trigger.py
@@ -1,4 +1,5 @@
 import sqlalchemy as sa
+from sqlalchemy import text
 
 from sqlalchemy_searchable import sync_trigger, vectorizer
 from tests import create_test_cases, TestCase
@@ -11,40 +12,46 @@ class SyncTriggerTestCase(TestCase):
     def create_tables(self):
         with self.engine.begin() as conn:
             conn.execute(
-                """
-                CREATE TABLE article (
-                    name TEXT,
-                    content TEXT,
-                    "current_user" TEXT,
-                    search_vector TSVECTOR
+                text(
+                    """
+                    CREATE TABLE article (
+                        name TEXT,
+                        content TEXT,
+                        "current_user" TEXT,
+                        search_vector TSVECTOR
+                    )
+                    """
                 )
-                """
             )
 
     def drop_tables(self):
         with self.engine.begin() as conn:
-            conn.execute("DROP TABLE article")
+            conn.execute(text("DROP TABLE article"))
 
     def test_creates_triggers_and_functions(self):
         with self.engine.begin() as conn:
             sync_trigger(conn, "article", "search_vector", ["name", "content"])
             conn.execute(
-                """INSERT INTO article (name, content)
-                VALUES ('some name', 'some content')"""
+                text(
+                    """INSERT INTO article (name, content)
+                    VALUES ('some name', 'some content')"""
+                )
             )
-            vector = conn.execute("SELECT search_vector FROM article").scalar()
+            vector = conn.execute(text("SELECT search_vector FROM article")).scalar()
         assert vector == "'content':4 'name':2"
 
     def test_updates_column_values(self):
         with self.engine.begin() as conn:
             sync_trigger(conn, "article", "search_vector", ["name", "content"])
             conn.execute(
-                """INSERT INTO article (name, content)
-                VALUES ('some name', 'some content')"""
+                text(
+                    """INSERT INTO article (name, content)
+                    VALUES ('some name', 'some content')"""
+                )
             )
-            conn.execute("ALTER TABLE article DROP COLUMN name")
+            conn.execute(text("ALTER TABLE article DROP COLUMN name"))
             sync_trigger(conn, "article", "search_vector", ["content"])
-            vector = conn.execute("SELECT search_vector FROM article").scalar()
+            vector = conn.execute(text("SELECT search_vector FROM article")).scalar()
         assert vector == "'content':2"
 
     def test_custom_vectorizers(self):
@@ -67,24 +74,28 @@ class SyncTriggerTestCase(TestCase):
                 metadata=self.Base.metadata,
             )
             conn.execute(
-                """INSERT INTO article (name, content)
-                VALUES ('some name', 'some bad content')"""
+                text(
+                    """INSERT INTO article (name, content)
+                    VALUES ('some name', 'some bad content')"""
+                )
             )
-            vector = conn.execute("SELECT search_vector FROM article").scalar()
+            vector = conn.execute(text("SELECT search_vector FROM article")).scalar()
         assert vector == "'content':5 'good':4 'name':2"
 
     def test_trigger_with_reserved_word(self):
         with self.engine.begin() as conn:
             conn.execute(
-                """INSERT INTO article (name, content, "current_user")
-                VALUES ('some name', 'some bad content', now())"""
+                text(
+                    """INSERT INTO article (name, content, "current_user")
+                    VALUES ('some name', 'some bad content', now())"""
+                )
             )
 
             sync_trigger(
                 conn, "article", "search_vector", ["name", "content", "current_user"]
             )
             # raises ProgrammingError without reserved_words:
-            conn.execute("""UPDATE article SET name=name""")
+            conn.execute(text("UPDATE article SET name=name"))
 
 
 create_test_cases(SyncTriggerTestCase)
