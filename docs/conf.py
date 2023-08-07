c59dfb3b5c997e7270c7ac9d096d22dd987e87c0
Janne Vanhala <jpvanhal@users.noreply.github.com>
2023-08-07 17:54:55 +0300

    build: use ruff for linting
    Use [Ruff][1] for linting. This replaces both isort and flake8.
    
    [1]: https://beta.ruff.rs/docs/

--- docs/conf.py
+++ docs/conf.py
@@ -11,13 +11,14 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import os
+import sys
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
-from sqlalchemy_searchable import __version__
+from sqlalchemy_searchable import __version__  # noqa: E402
 
 # -- General configuration -----------------------------------------------------
 
@@ -26,7 +27,15 @@ from sqlalchemy_searchable import __version__
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
